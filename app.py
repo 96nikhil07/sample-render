85a666ed91560492c09bcea33a30febf47a20ded
Pawar <NPAWAR8@jaguarlandrover.com>
2025-02-10 17:10:08 +0530

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])  # Define the route for the root URL ("/")
 def hello_world():
-    return "Welcome to Nikhil's World"
+    return "Welcome to Nikhil's World Hi hello"
 
 if __name__ == '__main__':
     app.run(debug=False)  # Set debug=False in production
