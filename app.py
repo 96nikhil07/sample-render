1482976d880e3776ad0a67d645e72081908ea9a7
Pawar <NPAWAR8@jaguarlandrover.com>
2025-02-10 15:45:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])  # Define the route for the root URL ("/")
+def hello_world():
+    return "Welcome to Nikhil's World"
+
+if __name__ == '__main__':
+    app.run(debug=False)  # Set debug=False in production
